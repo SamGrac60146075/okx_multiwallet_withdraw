89507aef343c4a1c74ea307c61e9b1a0df6b6dde
Angus <168@eatmyfit.anonaddy.com>
2023-02-22 19:42:14 +0800

    Add okex-api-v5 files

---
+++ okex-api-v5/README_ZH.md
@@ -0,0 +1,13 @@
+#### 安装相关所需库
+
+1.1 安装所需库
+* 到`okex-api-v5`目錄下
+```cmd
+python setup.py install
+```
+
+#### 第二步：配置个人信息
+
+2.1 如果还未有API，可[点击](https://www.okx.com/account/users/myApi)前往官网进行申请
+
+2.2 将各项信息在`example.py（RestAPI）`和`websocket_example.py（WebSocketAPI）`中填写
---
+++ okex-api-v5/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name='okex',
+    version='1.1.6',
+    packages=['okex'],
+    install_requires=['requests'],
+    author='zerodivision',
+    author_email='zerodivision@zerodivision.tech',
+    url='https://www.zerodivision.tech/'
+)
